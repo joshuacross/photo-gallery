b6a5dfe4f6a6b04a5fc4a200383650d01760d9a8
joshuacross <joshua.dexter.cross@gmail.com>
2017-12-23 20:26:01 +0000

    Default JPA repository implementation

---
+++ src/main/java/uk/co/joshuacross/photogallery/image/ImageRepository.java
@@ -0,0 +1,8 @@
+package uk.co.joshuacross.photogallery.image;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ImageRepository extends JpaRepository<ImageModel, Long>{
+}
