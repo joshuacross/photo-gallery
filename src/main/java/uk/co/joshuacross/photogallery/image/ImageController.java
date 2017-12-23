dd7626c95573de1aa8b5c3291a96b4a0e9c496a2
joshuacross <joshua.dexter.cross@gmail.com>
2017-12-23 20:26:20 +0000

    Post image route

---
+++ src/main/java/uk/co/joshuacross/photogallery/image/ImageController.java
@@ -0,0 +1,23 @@
+package uk.co.joshuacross.photogallery.image;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/api")
+public class ImageController {
+
+    @Autowired
+    FileSystemStorage fileSystemStorage;
+    @Autowired
+    ImageRepository imageRepository;
+
+    @PostMapping("/image")
+    public void createImage(@RequestParam("file") MultipartFile file) {
+        fileSystemStorage.store(file);
+//        return imageRepository.save(new ImageModel());
+    }
+}
