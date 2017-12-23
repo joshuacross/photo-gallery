abf7d9b90a5f5837de311ec638a424a0c43fee6a
joshuacross <joshua.dexter.cross@gmail.com>
2017-12-23 20:24:37 +0000

    Enable JPA auditing

--- src/main/java/uk/co/joshuacross/photogallery/PhotoGalleryApplication.java
+++ src/main/java/uk/co/joshuacross/photogallery/PhotoGalleryApplication.java
@@ -2,8 +2,10 @@ package uk.co.joshuacross.photogallery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class PhotoGalleryApplication {
 
 	public static void main(String[] args) {
