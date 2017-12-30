5416a0b35711e1bcc3ce58787c79ab88004799b8
joshuacross <joshua.dexter.cross@gmail.com>
2017-12-30 20:54:44 +0000

    Implementation for uploading an image blob to cloudinary and persisting the url and metadata

--- src/main/java/uk/co/joshuacross/photogallery/image/ImageController.java
+++ src/main/java/uk/co/joshuacross/photogallery/image/ImageController.java
@@ -1,23 +1,35 @@
 package uk.co.joshuacross.photogallery.image;
 
+import com.cloudinary.Singleton;
+import com.cloudinary.utils.ObjectUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import javax.validation.Valid;
+import java.io.IOException;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/")
 public class ImageController {
 
     @Autowired
-    FileSystemStorage fileSystemStorage;
-    @Autowired
     ImageRepository imageRepository;
 
     @PostMapping("/image")
-    public void createImage(@RequestParam("file") MultipartFile file) {
-        fileSystemStorage.store(file);
-//        return imageRepository.save(new ImageModel());
+    public ImageModel createImage(@RequestParam("file") MultipartFile file) throws IOException {
+        Map uploadResult = Singleton.getCloudinary().uploader().upload(file.getBytes(), ObjectUtils.asMap("resource_type", "auto"));
+
+        ImageModel image = new ImageModel();
+        image.setId((String) uploadResult.get("public_id"));
+        image.setName((String) uploadResult.get("original_filename"));
+        image.setUrl((String) uploadResult.get("url"));
+        image.setContentType((String) uploadResult.get("resource_type"));
+        image.setSize((Integer) uploadResult.get("bytes"));
+
+        return imageRepository.save(image);
     }
 }
