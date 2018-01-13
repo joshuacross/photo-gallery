584284c6e179209ff3b96fe3159a99c315b95a88
joshuacross <joshua.dexter.cross@gmail.com>
2018-01-13 19:18:08 +0000

    Added routes to get all images, get a specified image and delete an image

--- src/main/java/uk/co/joshuacross/photogallery/image/ImageController.java
+++ src/main/java/uk/co/joshuacross/photogallery/image/ImageController.java
@@ -3,13 +3,13 @@ package uk.co.joshuacross.photogallery.image;
 import com.cloudinary.Singleton;
 import com.cloudinary.utils.ObjectUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -20,7 +20,7 @@ public class ImageController {
     ImageRepository imageRepository;
 
     @PostMapping("/image")
-    public ImageModel createImage(@RequestParam("file") MultipartFile file) throws IOException {
+    public ImageModel postImage(@RequestParam("file") MultipartFile file) throws IOException {
         Map uploadResult = Singleton.getCloudinary().uploader().upload(file.getBytes(), ObjectUtils.asMap("resource_type", "auto"));
 
         ImageModel image = new ImageModel();
@@ -31,5 +31,35 @@ public class ImageController {
         image.setSize((Integer) uploadResult.get("bytes"));
 
         return imageRepository.save(image);
+    }
+
+    @GetMapping("/image")
+    public List<ImageModel> getImages() {
+        return imageRepository.findAll();
+    }
+
+    @GetMapping("/image/{id}")
+    public ResponseEntity<ImageModel> getImage(@PathVariable(value = "id") String id) {
+        ImageModel image = imageRepository.findOne(id);
+
+        if (image == null) {
+            return ResponseEntity.notFound().build();
+        }
+
+        return ResponseEntity.ok().body(image);
+    }
+
+    @DeleteMapping("/image/{id}")
+    public ResponseEntity<ImageModel> deleteImage(@PathVariable(value = "id") String id) throws Exception {
+        ImageModel image = imageRepository.findOne(id);
+
+        if (image == null) {
+            return ResponseEntity.notFound().build();
+        }
+
+        imageRepository.delete(image);
+        Singleton.getCloudinary().api().deleteResources(Arrays.asList(image.getId()), ObjectUtils.emptyMap());
+
+        return ResponseEntity.ok().build();
     }
 }
