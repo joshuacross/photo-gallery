b012848de7bb81e624c101a9a088426fe4263f49
joshuacross <joshua.dexter.cross@gmail.com>
2018-01-13 19:15:36 +0000

    Changed id type to be a string because the table pk is a string based on the public id supplied by Cloudinary

--- src/main/java/uk/co/joshuacross/photogallery/image/ImageRepository.java
+++ src/main/java/uk/co/joshuacross/photogallery/image/ImageRepository.java
@@ -4,5 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ImageRepository extends JpaRepository<ImageModel, Long>{
+public interface ImageRepository extends JpaRepository<ImageModel, String>{
 }
